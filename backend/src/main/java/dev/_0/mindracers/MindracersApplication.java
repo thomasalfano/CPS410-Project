0852d7afb74b556b90f40ab4e287bff035694885
Thomas Alfano <alfano1tj@alma.edu>
2024-10-02 10:40:50 -0400

    intialize SpringBoot

---
+++ backend/src/main/java/dev/_0/mindracers/MindracersApplication.java
@@ -0,0 +1,13 @@
+package dev._0.mindracers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindracersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindracersApplication.class, args);
+	}
+
+}
