2f988ff4a24d3ab38122b361f968c66cd6a31ff2
adderflight <adderflight1@protonmail.com>
2024-10-09 10:28:46 -0400

    fixed sql errors and added comments to sample sql script

--- backend/src/main/java/dev/_0/mindracers/example/testdatamysql/sample-sql-script.sql
+++ backend/src/main/java/dev/_0/mindracers/example/testdatamysql/sample-sql-script.sql
@@ -1,13 +1,24 @@
-USE test-db-1;
+-- drop db to wipe testdb1
+DROP DATABASE testdb1;
 
+-- create testdb1 for testing
+CREATE DATABASE testdb1;
+
+-- use testdb1 as the main db to run sql commands against
+USE testdb1;
+
+-- drop student_info table to wipe all values
 DROP TABLE student_info;
 
+-- create student_info table
 CREATE TABLE student_info(
     student_id      INT         AUTO_INCREMENT PRIMARY KEY,
     first_name      VARCHAR(20) NOT NULL,
     last_name       VARCHAR(30) NOT NULL,
     grade_level     VARCHAR(10)
 );
+
+/* example sql queries */
 
 SELECT * FROM student_info;
 
