d7f46d976c7a2c83a0bf86b1154ae2d1c35e98e9
zharing02 <harin1zp@cmich.edu>
2024-10-09 21:29:40 -0400

    Transferring old HTML code to React (front-end currently runs using 'npm run start')

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -1,25 +1,45 @@
-import logo from "../logo.svg";
-import "./App.css";
+import React from 'react';
+import './App.css'; // Importing the CSS file
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h6>Mind Racers</h6>
+      </div>
+      <div className="signup-form">
+        <form id="form" onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" required />
+
+          <label htmlFor="email2">Confirm Email</label>
+          <input type="email" id="email2" required />
+
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" required />
+
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" required />
+
+          <label htmlFor="password2">Re-Enter Password</label>
+          <input type="password" id="password2" required />
+
+          <button className="submit-button" type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 }
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const email = document.getElementById('email').value;
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  console.log(email);
+  console.log(username);
+  console.log(password);
+};
 
 export default App;
